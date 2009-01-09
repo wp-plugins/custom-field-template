6a27cc0c56dc548a1c0bb5109eb39b08057d397a
Hiroaki Miyashita <Hiroaki Miyashita@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-09 12:15:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/custom-field-template/trunk@85415 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-field-template.php
+++ custom-field-template.php
@@ -4,7 +4,7 @@ Plugin Name: Custom Field Template
 Plugin URI: http://wordpressgogo.com/development/custom-field-template.html
 Description: This plugin adds the default custom fields on the Write Post/Page.
 Author: Hiroaki Miyashita
-Version: 0.8.0
+Version: 0.8
 Author URI: http://wordpressgogo.com/
 */
 
